b80e329ac1dc9643caddc7756bba2f5bb3791c6e
lukecanny <lukecanny@protonmail.ch>
2022-11-22 11:22:28 +0000

    Added test 8 template code - Test incomplete

--- EE451_Breakout_AnthonyBird_LukeCanny.s
+++ EE451_Breakout_AnthonyBird_LukeCanny.s
@@ -966,6 +966,50 @@ testbench7:
   addi x15, x0, 2         # low count delay, for testing 
   
   # Wall
+  # xori  x16,  x0,   -1    # wall x16 = 0xffffffff
+  lui   x16,  0x00010     # matches ballVec 0b0000 0000 0000 0001 0000 0000 0000 0000 
+  
+  # Ball
+  lui   x17,  0x00010     # ballVec 0b0000 0000 0000 0001 0000 0000 0000 0000 
+  addi  x18,  x0,   16    # CSBallXAdd (4:0)
+  addi  x19,  x0,   16    # NSBallXAdd (4:0)
+  addi  x20,  x0,   52    # CSBallYAdd (4:0)		(Address 52 = Row 13)	
+  addi  x21,  x0,   52    # NSBallYAdd (4:0)		(Address 52 = Row 13)
+  addi  x22,  x0,   4     # CSBallDir  (2:0) = 100 = N 
+  addi  x23,  x0,   4 	  # NSBallDir  (2:0) = 100 = N
+  addi  x24,  x0,   1     # ballNumDlyCount (4:0)
+  
+  # Paddle
+  lui   x25,  0x0007c     # paddleVec 0b0000 0000 0000 0111 1100 0000 0000 0000 = 0x0007c000
+  addi  x26,  x0,   5     # paddleSize
+  addi  x27,  x0,   14    # paddleXAddLSB      
+  addi  x28,  x0,   1     # paddleNumDlyCount 
+  
+  # BONUS: Static Paddles
+  lui   x31,  0x1fc00
+  addi  x31,  x31,  0x3f8
+  addi  x5,   x0,   0
+  sw    x31,  44(x5)
+  
+  # Score
+  addi  x29,  x0,   31     # score
+  addi x30,   x0,   3     # lives 
+  jalr  x0,   0(x1)       # ret
+
+# =================================== Test 8 Description ============================= #
+# Test Number:				8
+# Description:				Player reaches score of X
+#                     The speed of the ball will double (to make game progressively 
+#                     more challenging)
+# =================================== End of Description ============================= #
+
+# !!!!!!!!!!!!!!!!!!!!!!!! INCOMPLETE - Only template code in place !!!!!!!!!!!!!!!!!! # 
+testbench8:  
+	lui   x15,  0x98968     # 0x98968000 
+  srli  x15,  x15,  9	    # 0x00098968 
+  addi x15, x0, 2         # low count delay, for testing 
+  
+  # Wall
   xori  x16,  x0,   -1    # wall x16 = 0xffffffff
   
   # Ball
